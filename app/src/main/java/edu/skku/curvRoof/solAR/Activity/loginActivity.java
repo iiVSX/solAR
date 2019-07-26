5cc5d04d1d4a5cedc1f1ac4fa3ad440852fba49d
iiVSX <matthew17101@naver.com>
2019-07-26 16:00:24 +0900

    Constraint Layout으로 login_activity.xml 비율 고침, landscape가 horizontal일 때도 바꾸길 요망(채은)
    Firebase Authentication을 이용한 로그인 기능 구현

--- app/src/main/java/edu/skku/curvRoof/solAR/Activity/loginActivity.java
+++ app/src/main/java/edu/skku/curvRoof/solAR/Activity/loginActivity.java
@@ -1,41 +1,126 @@
 package edu.skku.curvRoof.solAR.Activity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import edu.skku.curvRoof.solAR.R;
 
 public class loginActivity extends AppCompatActivity {
-    private Button loginBtn;
+    private Button loginBtn, registerBtn;
     private EditText idEt, pwdEt;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+
         loginBtn = (Button)findViewById(R.id.loginBtn);
         idEt = (EditText)findViewById(R.id.idEt);
         pwdEt = (EditText)findViewById(R.id.pwdEt);
+        registerBtn = (Button)findViewById(R.id.registerBtn);
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!idEt.getText().toString().trim().equals("") && !pwdEt.getText().toString().trim().equals("")){
-                    Intent intent = new Intent(loginActivity.this, MainActivity.class);
-                    intent.putExtra("ID", idEt.getText().toString());
-                    intent.putExtra("password", pwdEt.getText().toString());
-                    startActivity(intent);
+                String email = idEt.getText().toString();
+                String password = pwdEt.getText().toString();
+                if(!email.trim().equals("") && !password.trim().equals("")){
+                    signIn(email, password);
                 }
                 else{
                     idEt.setText("");
                     pwdEt.setText("");
                     Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder newAccountDialog = new AlertDialog.Builder(loginActivity.this);
+                newAccountDialog.setTitle("Input Account Information");
+                LayoutInflater inflater = getLayoutInflater();
+                View custom_view = inflater.inflate(R.layout.dialog_register, null);
+                newAccountDialog.setView(custom_view);
+
+                EditText newEmailEt = custom_view.findViewById(R.id.newEmailEt);
+                EditText newPwdEt = custom_view.findViewById(R.id.newPwdEt);
+
+                String email = newEmailEt.getText().toString();
+                String password = newPwdEt.getText().toString();
+                if(!email.trim().equals("") && !password.trim().equals("")){
+                    createAccount(email, password);
+                }
+                else{
+                    newEmailEt.setText("");
+                    newPwdEt.setText("");
+                    Toast.makeText(getApplicationContext(), "Register Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    public void createAccount(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Intent intent = new Intent(loginActivity.this, MainActivity.class);
+                    try{
+                        intent.putExtra("ID", user.getEmail());
+                    }catch(NullPointerException e){
+                        Toast.makeText(getApplicationContext(), "Wrong Email", Toast.LENGTH_SHORT).show();
+                    }
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(loginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void signIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Intent intent = new Intent(loginActivity.this, MainActivity.class);
+                    try{
+                        intent.putExtra("ID", user.getEmail());
+                    }catch(NullPointerException e){
+                        Toast.makeText(getApplicationContext(), "Wrong Email", Toast.LENGTH_SHORT).show();
+                    }
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(loginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
                 }
             }
         });
