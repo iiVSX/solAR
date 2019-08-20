98fab4988b221b1cd3733f5ea1073e331553bbac
taehyun <adam990901@gmail.com>
2019-08-20 11:50:04 +0900

    2019. 8. 19 Done: - Model/Plane.java(draw,bufferUpdate 함수 추가) - plane.vert, plane.frag shader 코드 추가

---
+++ app/src/main/assets/plane.vert
@@ -0,0 +1,6 @@
+attribute vec4 vPosition;
+uniform mat4 uMVPMatrix;
+
+void main(){
+    gl_Position = uMVPMatrix * vPosition;
+}
--- app/src/main/java/edu/skku/curvRoof/solAR/Model/Plane.java
+++ app/src/main/java/edu/skku/curvRoof/solAR/Model/Plane.java
@@ -1,16 +1,56 @@
 package edu.skku.curvRoof.solAR.Model;
 
+import android.content.Context;
+import android.opengl.GLES20;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.util.HashMap;
+
+import edu.skku.curvRoof.solAR.Utils.ShaderUtil;
+
 public class Plane {
     private Point ll,lr,ul,ur;
     private float[] normal = new float[3];
 
-    public Plane(Point ll, Point lr, Point ul, Point ur){
+    private float[] planeVertex;
+
+    private static final String VERTEX_SHADER_NAME = "plane.vert";
+    private static final String FRAGMENT_SHADER_NAME = "plane.frag";
+    private int vertexShader;
+    private int fragmentShader;
+
+    private int mProgram;
+
+    private int mPosition;
+    private int mColor_u;
+    private int uMVPMatrixHandle;
+
+    private static final int COORDS_PER_VERTEX = 4;
+    private static final int FLOAT_SIZE = 4;
+
+    private FloatBuffer vertexBuffer;
+
+    public Plane(Point ll, Point lr, Point ur, Point ul){
 
         // Duplicate 4 points
         this.ll = ll;
         this.lr = lr;
         this.ul = ul;
         this.ur = ur;
+
+        planeVertex = new float[] {
+                ll.getX(), ll.getY(), ll.getZ(),
+                lr.getX(), lr.getY(), lr.getZ(),
+                ul.getX(), ul.getY(), ul.getZ(),
+
+                ul.getX(), ul.getY(), ul.getZ(),
+                lr.getX(), lr.getY(), lr.getZ(),
+                ur.getX(), ur.getY(), ur.getZ(),
+
+        };
 
         this.calNormal();
     }
@@ -62,6 +102,59 @@ public class Plane {
         this.normal[0] = vec1[1]*vec2[2] - vec1[2]*vec2[1];
         this.normal[1] = vec1[2]*vec2[0] - vec1[0]*vec2[2];
         this.normal[2] = vec1[0]*vec2[1] - vec1[1]*vec2[0];
+    }
+
+    public void bufferUpdate(){
+        planeVertex = new float[] {
+                ll.getX(), ll.getY(), ll.getZ(),
+                lr.getX(), lr.getY(), lr.getZ(),
+                ul.getX(), ul.getY(), ul.getZ(),
+
+                ul.getX(), ul.getY(), ul.getZ(),
+                lr.getX(), lr.getY(), lr.getZ(),
+                ur.getX(), ur.getY(), ur.getZ(),
+
+        };
+
+        ByteBuffer bb = ByteBuffer.allocateDirect(planeVertex.length * FLOAT_SIZE);
+        bb.order(ByteOrder.nativeOrder());
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(planeVertex);
+        vertexBuffer.position(0);
+    }
+
+    public void createGlThread(Context context) throws IOException {
+
+
+        vertexShader = ShaderUtil.loadGLShader("Plane", context, GLES20.GL_VERTEX_SHADER, VERTEX_SHADER_NAME);
+        fragmentShader = ShaderUtil.loadGLShader("Plane", context, GLES20.GL_FRAGMENT_SHADER, FRAGMENT_SHADER_NAME);
+
+        //bind shader's variable position
+        mProgram = GLES20.glCreateProgram();
+        GLES20.glAttachShader(mProgram, vertexShader);
+        GLES20.glAttachShader(mProgram, fragmentShader);
+        GLES20.glLinkProgram(mProgram);
+        GLES20.glUseProgram(mProgram);
+
+        mPosition = GLES20.glGetAttribLocation(mProgram, "vPosition");
+        mColor_u = GLES20.glGetUniformLocation(mProgram, "u_Color");
+        uMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+
+    }
+
+    public void draw(float[] vpMatrix){
+        GLES20.glUseProgram(mProgram);
+
+        GLES20.glEnableVertexAttribArray(mPosition);
+
+        GLES20.glVertexAttribPointer(mPosition, FLOAT_SIZE, GLES20.GL_FLOAT, false, COORDS_PER_VERTEX * FLOAT_SIZE, vertexBuffer);
+        GLES20.glUniformMatrix4fv(uMVPMatrixHandle, 1, false, vpMatrix, 0);
+
+        GLES20.glUniform4f(mColor_u, 0.0f, 0.0f, 1.0f, 1.0f);
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexBuffer.remaining()/4);
+        GLES20.glDisableVertexAttribArray(mPosition);
+
     }
 
 }
