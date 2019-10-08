65e7e20815df7ff3a96c5b7663a9fb0b0ce1a81c
SeolChaeEun <eun476@naver.com>
2019-10-09 02:34:00 +0900

    설치 면적을 계산중입니다 화면 생성

---
+++ app/src/main/java/edu/skku/curvRoof/solAR/calculateSplashActivity.java
@@ -0,0 +1,13 @@
+package edu.skku.curvRoof.solAR;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class calculateSplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate_splash);
+    }
+}
