12c011e1e2a199c3751aa394add8268c56af3f09
SeolChaeEun <eun476@naver.com>
2019-10-09 00:48:49 +0900

    갤럭시 S7 깨짐 현상 해결??? 움직이시오 안내문 java xml 생성

---
+++ app/src/main/java/edu/skku/curvRoof/solAR/moveSplashActivity.java
@@ -0,0 +1,13 @@
+package edu.skku.curvRoof.solAR;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class moveSplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_splash);
+    }
+}
