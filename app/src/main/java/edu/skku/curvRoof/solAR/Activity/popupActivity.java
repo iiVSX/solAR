3d11873193f8d30c84742158fb67e1efbc3dd8b7
SeolChaeEun <eun476@naver.com>
2019-11-07 17:48:59 +0900

    popupActivity ㅛ생성

---
+++ app/src/main/java/edu/skku/curvRoof/solAR/Activity/popupActivity.java
@@ -0,0 +1,15 @@
+package edu.skku.curvRoof.solAR.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.skku.curvRoof.solAR.R;
+
+public class popupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup);
+    }
+}
